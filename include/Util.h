bc021457975b0a63cd5915d06e8bd22bbf30045e
vis4rd <alagaesia.vrael@gmail.com>
2021-07-22 20:24:12 +0200

    moved most function definitions to .cpp file

--- include/Util.h
+++ include/Util.h
@@ -6,37 +6,9 @@ namespace ecs
 {
 namespace util
 {
-	// Source: https://en.cppreference.com/w/cpp/string/basic_string/replace
-	std::size_t replace_all(std::string& inout, std::string_view what, std::string_view with)
-	{
-		std::size_t count{};
-		for(std::string::size_type pos{};
-			inout.npos != (pos = inout.find(what.data(), pos, what.length()));
-			pos += with.length(), ++count)
-		{
-			inout.replace(pos, what.length(), with.data(), with.length());
-		}
-		return count;
-	}
-	 
-	std::size_t remove_all(std::string& inout, std::string_view what)
-	{
-		return replace_all(inout, what, "");
-	}
-	//
-	bool remove_string(std::string& inout, std::string_view what)
-	{
-		std::string::size_type pos{};
-		if(inout.npos != (pos = inout.find(what.data(), pos, what.length())))
-		{
-			inout.replace(pos, what.length(), "", 0);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+	std::size_t replace_all(std::string& inout, std::string_view what, std::string_view with);
+	std::size_t remove_all(std::string& inout, std::string_view what);
+	bool remove_string(std::string& inout, std::string_view what);
 
 	template <typename T>
 	constexpr auto type_name_to_string() noexcept
