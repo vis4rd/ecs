b4632d2b69dfd84bc32b2adfcf3eeb8c9b9a2b6c
vis4rd <alagaesia.vrael@gmail.com>
2021-07-24 22:41:27 +0200

    moved method definitions of Method class to the header file

--- include/Manager.h
+++ include/Manager.h
@@ -19,6 +19,8 @@ public:
 	// Manager assumes that the user knows the order of components in the pool.
 	// Most-important bit represents the first component in the pool.
 	void addEntity(const uint64 components, const uint64 flags);
+	void addComponent(const uint16 comp_dec_index, const uint64 entity_id);
+	void deleteEntity(const uint64 entity_id);
 
 private:
 	std::vector<Entity> m_entityBuffer;  // stores all entities
@@ -31,5 +33,102 @@ private:
 	uint64 m_entityCount;  // number of currently existing entities
 	// uint64 m_disposedEntityCount;  // number of destroyed entities in m_disposedEntityPool
 };
+
+template <typename TypeListT>
+Manager<TypeListT>::Manager(const uint64 max_entity_count)
+:
+m_flagCount(uint8{0}),
+m_componentCount(meta::TypeListSize<TypeListT>),
+m_maxEntityCount(max_entity_count),
+m_entityCount(uint64{0})
+// m_disposedEntityCount(uint64{0})
+{
+	if(m_entityBuffer.capacity() < m_maxEntityCount)
+	{
+		m_entityBuffer.reserve(m_maxEntityCount);
+	}
+	if(m_entityFlags.capacity() < m_maxEntityCount)
+	{
+		m_entityFlags.reserve(m_maxEntityCount);
+	}
+}
+
+template <typename TypeListT>
+void Manager<TypeListT>::initComponentBuffer()
+{
+	m_componentBuffer = ComponentBuffer<TypeListT>{};
+}
+
+template <typename TypeListT>
+void Manager<TypeListT>::addEntity(const uint64 components, const uint64 flags)
+{
+	if(m_entityCount < m_maxEntityCount)
+	{
+		m_entityCount++;
+		m_entityBuffer.emplace_back(Entity());
+
+		// parsing components
+		uint16 bitpos = 0;
+		for(uint64 c = uint64{1} << 63; c >= uint64{1 << 0}, bitpos < 64; c = (c >> 1), bitpos++)
+		{
+			if(bitpos < m_componentCount)
+			{
+				m_componentBuffer.template addComponentByIndex(bitpos, m_entityBuffer.back().getID());
+			}
+			else
+			{
+				std::cout << "[WARNING] Component bit position has exceeded " <<
+					"known component count (position: " << bitpos << ", component count: " <<
+					m_componentCount << ") - breaking operation..." << std::endl;
+				break;
+			}
+		}
+
+		// adding flags
+		m_entityFlags.push_back(flags);
+
+		// TBD : use disposed entities instead of assigning new ones everytime
+	}
+	else
+	{
+		std::cout << "[WARNING] Number of allocated entities has reached the cap," <<
+			" ignoring the new ones..." << std::endl;
+	}
+}
+
+template <typename TypeListT>
+void Manager<TypeListT>::addComponent(const uint16 comp_dec_index, const uint64 entity_id)
+{
+	if(m_componentBuffer.getComponentByIndex<comp_dec_index>(entity_id))
+	{
+		std::cout << "[WARNING] Given component already exists under " << 
+			"passed Entity ID - " <<std::endl <<"ignoring void Manager<TypeListT>::addComponent" <<
+			"(const uint16 comp_dec_index, const uint64 entity_id)" << std::endl;
+		return;
+	}
+	else
+	{
+		m_componentBuffer.addComponentByIndex<comp_dec_index>(entity_id);
+	}
+}
+
+template <typename TypeListT>
+void Manager<TypeListT>::deleteEntity(const uint64 entity_id)
+{
+	auto e = m_entityBuffer.begin();
+	for(; e < m_entityBuffer.end(); e++)
+	{
+		if(e->getID() == entity_id)
+		{
+			break;
+		}
+	}
+	m_componentBuffer.removeComponents(e->getID());
+	auto pos = e - m_entityBuffer.begin();
+	std::swap(*e, m_entityBuffer.back());
+	m_entityBuffer.pop_back();
+	std::swap(m_entityFlags[pos], m_entityFlags.back());
+	m_entityFlags.pop_back();
+}
 
 }  // namespace ecs
--- main.cpp
+++ main.cpp
@@ -6,81 +6,57 @@
 
 int main()
 {
-    ecs::Entity ent;
-    // ecs::Manager mgr;
-
     struct C0{};
     struct C1{};
     struct C2{};
     struct C3{};
+    struct C4{};
+    struct C5{};
+    struct C6{};
+    struct C7{};
+    struct C8{};
+    struct C9{};
+    struct D0{};
+    struct D1{};
+    struct D2{};
+    struct D3{};
+    struct D4{};
+    struct D5{};
+    struct D6{};
+    struct D7{};
+    struct D8{};
+    struct D9{};
+    struct E0{};
+    struct E1{};
+    struct E2{};
+    struct E3{};
+    struct E4{};
+    struct E5{};
+    struct E6{};
+    struct E7{};
+    struct E8{};
+    struct E9{};
 
-    using Types = ecs::meta::ComponentPool<float, C1, C2, C3, int, char, std::vector<std::string>>;
+    using CP = ecs::meta::ComponentPool<C0, C1, C2, C3, C4, C5, C6, C7, C8, C9,
+        D0, D1, D2, D3, D4, D5, D6, D7, D8, D9,
+        E0, E1, E2, E3, E4, E5, E6, E7, E8, E9>;
 
-    std::cout << "SIZE = " << ecs::meta::TypeListSize<Types> << std::endl;
-    std::cout << "INDEX = " << ecs::meta::IndexOf<C1, Types> << std::endl;
-    ecs::meta::metautil::Print<Types>();
-
-    //
-    std::cout << std::endl;
-    //
-
-    struct F1
+    ecs::Manager<CP> manager;
+    manager.initComponentBuffer();
+    for(ecs::uint32 i = 0; i < ecs::uint32{1000}; i++)
     {
-        int value = 5;
-    };
-    using Test = ecs::meta::ComponentPool<F1>;
-    ecs::ComponentBuffer<Test> CB;
-    auto &f = CB.addComponent<F1>(ecs::uint64{1});
-    f.value = 11;
-    std::cout << CB.getComponent<F1>(1).value << std::endl;
+        manager.addEntity(ecs::uint64{0x3FFFFFFF}, ecs::uint64{1});
+    }
 
-    //
-    std::cout << std::endl;
-    //
+    for(ecs::uint32 i = ecs::uint32{0}; i < ecs::uint32{1000}; i++)
+    {
+        manager.deleteEntity(ecs::uint64(i));
+    }
 
-    // TBD : simplify the examples and move them to ComponentBuffer.h as documentation ////////////
+    std::cout << "SIZE = " << ecs::meta::TypeListSize<CP> << std::endl;
+    std::cout << "INDEX = " << ecs::meta::IndexOf<C1, CP> << std::endl;
+    // ecs::meta::metautil::Print<Types>();
 
-    using Test2 = ecs::meta::ComponentPool<int, float, char, long long, double>;
-    ecs::ComponentBuffer<Test2> CB2;
-    // entity 1 = int + float + char
-    // entity 2 = int + long long + double
-    auto &ref = CB2.addComponent<int>(ecs::uint64{1});
-    auto &ref2 = CB2.addComponent<float>(ecs::uint64{1});
-    auto &ref3 = CB2.addComponent<char>(ecs::uint64{1});
-    ref = 5;
-    ref2 = 15.f;
-    ref3 = 'c';
-    CB2.addComponent<double>(ecs::uint64{2});  // = 0
-    CB2.addComponent<long long>(ecs::uint64{2});
-    auto &hak = CB2.addComponent<int>(ecs::uint64{2});
-    hak = 2;
-    std::cout << "CB2 SIZE = " << CB2.size() << std::endl;
-    CB2.printAll();
-    auto tuple = CB2.getComponentsMatching<int>(1);
-    auto tuple2 = CB2.getComponentsMatching<int>(2);
-    std::cout << std::get<0>(tuple) << std::endl;
-    std::cout << std::get<0>(tuple2) << std::endl;
-
-    auto tuple3 = CB2.getComponentsMatching<>(1);
-    // std::cout << std::get<0>(tuple3) << std::endl;  // compilation error - tuple3 contains no types as none were provided for search
-
-    // auto tuple4 = CB2.getComponentsMatching<long long>(1);  // exception - there's no such component with given id
-
-    // auto tuple5 = CB2.getComponentsMatching<float>(3);  // exception - there's no such entity id introduced to the buffer
-
-    // auto vec1 = CB2.getComponentBucket<unsigned>();  // exception - there's no such component in ComponentPool
-
-    // auto &com1 = CB2.getComponent<unsigned>(1);  // exception - there's no such component in ComponentPool
-
-    // auto &com2 = CB2.getComponent<float>(3);  // exception - there's no such entity id introduced to the buffer
-
-    // auto &com3 = CB2.getComponent<double>(1);  // exception - there's no such component with given id
-
-    auto &com5 = CB2.getComponent<int>(2);
-    std::cout << com5 << std::endl;
-    CB2.removeComponents(2);
-    CB2.removeComponent<char>(ecs::uint64{1});
-    // entity 1 = int + float
-    std::cout << "CB2 SIZE = " << CB2.size() << std::endl;
+    
     return 0;
 }
