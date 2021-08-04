67679c8c880a84fa5b0df5f3214a037e7fe0954e
vis4rd <alagaesia.vrael@gmail.com>
2021-08-04 19:43:17 +0200

    setup time measurements to check ECS performance

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 #include "include/Manager.h"
 #include "include/ComponentBuffer.h"
 
-void test_fun1(int &arg1)
-{
-    arg1 = 5;
-}
-
 struct C0{};
 struct C1{};
 struct C2{};
@@ -40,13 +35,22 @@ struct E7{};
 struct E8{};
 struct E9{};
 
-void test_fun2(C9 &arg1)
+void test_fun1(int &arg1)
 {
-    arg1.data = 1337;
+    static int d;
+    arg1 = d++;
 }
 
-int main()
+void test_fun2(C9 &arg1)
 {
+    static int b;
+    arg1.data = b++;
+}
+
+float measure_time(bool &&suppressed = false)
+{
+    auto clock = std::chrono::high_resolution_clock::now();
+
     using CP = ecs::meta::ComponentPool<int, float, char, C3, C4, C5, C6, C7, C8, C9,
         D0, D1, D2, D3, D4, D5, D6, D7, D8, D9,
         E0, E1, E2, E3, E4, E5, E6, E7, E8, E9>;
@@ -57,32 +61,48 @@ int main()
         manager.addEntity<30>(ecs::uint64{0xFFFFFFFFFFFFFFFF}, ecs::uint64{1});
     }
 
-    auto &comp1 = manager.getComponent<9>(200);
-    comp1.data = 2;
+    /*auto &comp1 = manager.getComponent<9>(200);
+    comp1.data = 2;*/
     manager.checkComponent<9>(200);
 
     // THE GRAND TEST OF SYSTEM SUPPORT
     std::function<void(int &)> fun = test_fun1;
     std::function<void(C9 &)> fun2 = test_fun2;
+    std::function<void(char &)> fun3 = [&](char &c){ c = 'c'; };
     manager.applySystem<int>(fun);
     manager.applySystem<C9>(fun2);
-    std::cout << manager.getComponent<int>(1) << std::endl;
-    std::cout << manager.getComponent<C9>(789).data << std::endl;
-    std::function<void(char &)> fun3 = [&](char &c){ c = 'c'; };
     manager.applySystem<char>(fun3);
 
     manager.applySystem<int>(test_fun1);
     manager.applySystem<C9>(test_fun2);
     //
 
-    for(ecs::uint32 i = ecs::uint32{0}; i < ecs::uint32{1000}; i++)
-    {
-        manager.deleteEntity(ecs::uint64(i));
-    }
+    manager.deleteAllEntities();
 
-    std::cout << "SIZE = " << ecs::meta::TypeListSize<CP> << std::endl;
-    std::cout << "INDEX = " << ecs::meta::IndexOf<C4, CP> << std::endl;
-    // ecs::meta::metautil::Print<Types>();
+    /*std::cout << "SIZE = " << ecs::meta::TypeListSize<CP> << std::endl;
+    std::cout << "INDEX = " << ecs::meta::IndexOf<C4, CP> << std::endl;*/
+
+    auto clock2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(clock2 - clock);
+    static unsigned cycle;
+    if(!suppressed)
+        std::cout << "cycle (" << cycle++ << ") duration: " << duration.count()/1000.f << "ms" << std::endl;
+    return duration.count()/1000.f;
+}
+
+int main(int argc, char **argv)
+{
+    int count = (argc > 1) ? std::__cxx11::stoi(argv[1]) : 100;
+    std::vector<float> times;
+    for(int i = 0u; i < count; i++)
+    {
+        times.emplace_back(measure_time());
+    }
+    times.shrink_to_fit();
+    std::cout << std::endl;
+    std::cout << "average time: " << std::accumulate(times.begin(), times.end(), 0.f) / times.size() << "ms" << std::endl;
+    std::cout << "max: " << *std::max_element(times.begin(), times.end()) << "ms" << std::endl;
+    std::cout << "min: " << *std::min_element(times.begin(), times.end()) << "ms" << std::endl;
     
     return 0;
 }
